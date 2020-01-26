d5625936acdf7f560b893f1de7271d7b76f6f4f6
ariffirmanfirdaus <51688579+ariffirmanfirdaus@users.noreply.github.com>
2020-01-26 10:28:51 +0700

    Add files via upload

---
+++ Diesein.sh
@@ -0,0 +1,13 @@
+nano .bashrc
+clear
+cowsay -f eyes 'wellcom to My Script
+
+Username : Mr.ARIF.F.F
+
+TEAM: CYBER Hacker
+
+No:081213673380
+
+Imail:ariffirmanfirdaus111@gmail.com
+
+Youtube chanel : ARIF.F.F OFFLINE' | lolcat
